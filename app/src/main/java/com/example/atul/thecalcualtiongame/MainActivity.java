96af5baa9ba10e22e87308dd06a1a6ead893372f
Atul Rathore <kshtriya.atul@gmail.com>
2017-03-22 02:24:44 +0530

    4th Commit random number generation, score update, play again feature.

--- app/src/main/java/com/example/atul/thecalcualtiongame/MainActivity.java
+++ app/src/main/java/com/example/atul/thecalcualtiongame/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.atul.thecalcualtiongame;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,45 +14,108 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     Button startButton;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button plaAgainBTN;
+    TextView resultTV;
+    TextView sumTV;
+    TextView pointsTV;
+    TextView timerTextView;
     ArrayList<Integer> answers = new ArrayList<Integer>();
     int locationOfRightAnswer;
+    int gameScore = 0;
+    int numberOfProblems = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startButton = (Button) findViewById(R.id.startGameButton);
-        TextView sumTV = (TextView) findViewById(R.id.sumTV);
+        sumTV = (TextView) findViewById(R.id.sumTV);
+        resultTV = (TextView) findViewById(R.id.resultTV);
+        pointsTV = (TextView) findViewById(R.id.pointsTV);
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        plaAgainBTN = (Button) findViewById(R.id.playAgainBTN);
+        generateQuestion();
 
-        Button button3= (Button) findViewById(R.id.button3);
-        Button button4= (Button) findViewById(R.id.button4);
-        Button button5= (Button) findViewById(R.id.button5);
-        Button button6= (Button) findViewById(R.id.button6);
+        playAgain(findViewById(R.id.playAgainBTN));
+    }
 
+
+    public void playAgain(View view){
+        gameScore = 0;
+        numberOfProblems = 0;
+        timerTextView.setText("30s");
+        pointsTV.setText("0/0");
+        resultTV.setText("");
+        plaAgainBTN.setVisibility(View.INVISIBLE);
+
+        generateQuestion();
+        new CountDownTimer(3100, 1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText(String.valueOf(millisUntilFinished / 1000) + "s");
+            }
+
+            @Override
+            public void onFinish() {
+                plaAgainBTN.setVisibility(View.VISIBLE);
+                timerTextView.setText("0");
+                resultTV.setText("Your Score" + Integer.toString(gameScore) + "/" +
+                        Integer.toString(numberOfProblems));
+            }
+        }.start();
+    }
+
+
+    /*For Generating a new question*/
+    public void generateQuestion(){
         Random rdm = new Random();
         int num1 = rdm.nextInt(26);
         int num2 = rdm.nextInt(26);
         sumTV.setText(Integer.toString(num1) + " + " + Integer.toString(num2));
         locationOfRightAnswer = rdm.nextInt(4);
-
+        answers.clear();
         int wrongAnswer;
-        for(int i=0; i<4; i++){
-            if(i==locationOfRightAnswer){
-                answers.add(num1+num2);
-            }else{
-                wrongAnswer= rdm.nextInt(41);
-                while(wrongAnswer == num1+num2){
-                    wrongAnswer= rdm.nextInt(41);
+        for (int i = 0; i < 4; i++) {
+            if (i == locationOfRightAnswer) {
+                answers.add(num1 + num2);
+            } else {
+                wrongAnswer = rdm.nextInt(41);
+                while (wrongAnswer == num1 + num2) {
+                    wrongAnswer = rdm.nextInt(41);
                 }
                 answers.add(wrongAnswer);
             }
         }
-
         button3.setText(Integer.toString(answers.get(0)));
         button4.setText(Integer.toString(answers.get(1)));
         button5.setText(Integer.toString(answers.get(2)));
         button6.setText(Integer.toString(answers.get(3)));
+    }
 
+
+    /*For Selecting the right answer and updating the score*/
+    public void selectAnswer(View view) {
+//        Log.i("Tag", (String) view.getTag());
+
+        if (view.getTag().toString().equals(Integer.toString(locationOfRightAnswer))) {
+            //Log.i("Correct","AAAAAA");
+            gameScore++;
+            resultTV.setText("Correct!");
+        } else {
+            resultTV.setText("Incorrect!");
+        }
+        numberOfProblems++;
+        pointsTV.setText(Integer.toString(gameScore) + "/" + Integer.toString(numberOfProblems));
+        generateQuestion();
     }
 
     public void startGame(View view) {
